f2ea459ed327fd68dcf59679b3e1ce9d0fed47a3
armin <arminkheirkhahan@gmail.com>
2020-04-09 02:52:44 +0430

    change site title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,16 +50,11 @@ export default {
   ],
 
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'رسا',
+    title: 'ٰرسا',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
