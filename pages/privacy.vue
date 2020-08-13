983ef135799f667fb53ec9f3ff4cec30cb85893d
armin <arminkheirkhahan@gmail.com>
2020-08-13 12:39:05 +0430

    change privacy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -11,40 +11,70 @@ section {
 </style>
 <template>
   <section>
-    <h1 style="text-align: center;">Resaa Doctor</h1>
+    <h1 style="text-align: center;">Jemssi Consulting</h1>
     <p>&nbsp;</p>
     <h2 style="padding-left: 30px;">Privacy policy</h2>
     <p style="padding-left: 30px;">&nbsp;</p>
     <h4 style="padding-left: 30px;">What information do we collect?</h4>
     <p style="padding-left: 30px;">
-      - The phone numbers you enter in "Blocked Numbers" and "Allowed Numbers" pages, or when you share a contact's phone number with Resaa Doctor app.
-      <br />- The voice you record and the text you enter as replies in Medical Test page.
-      <br />- Some information about your iPhone device such as iOS version, iPhone model, your country and language set in your phone, and also session timing of your activity with the app.
+      - The phone numbers you enter in "Blocked Numbers" and "Allowed Numbers" pages, or when
+      you share a contact's phone number with Jemssi Consulting app.
+      <br />- The voice you record and the text you enter as
+      replies in Medical Test page.
+      <br />- Some information about your iPhone device such as iOS version, iPhone model,
+      your country and language set in your phone, and also session timing of your activity with the app.
     </p>
     <h4 style="padding-left: 30px;">What do we use your information for?</h4>
     <p style="padding-left: 30px;">
-      Any information we collect from you and store, may be used for the following reasons:
-      <br />- So that you can have a better application usage experience, by enabling you login in in any other iPhone device you want with your current profile information.
-      <br />- Collecting your iPhone device information would help us to better serve you and address any problems you would have when using our app.
+      Any information we collect from you and store, may be used for the following
+      reasons:
+      <br />- So that you can have a better application usage experience, by enabling you login in in any other
+      iPhone device you want with your current profile information.
+      <br />- Collecting your iPhone device information would
+      help us to better serve you and address any problems you would have when using our app.
     </p>
     <h4 style="padding-left: 30px;">Advertisement in Application</h4>
-    <p
-      style="padding-left: 30px;"
-    >We don't use any information from you nor have we used any advertisement SDKs or services in our app for advertisement purposes.</p>
+    <p style="padding-left: 30px;">
+      We don't use any information from you nor have we used any advertisement SDKs or services
+      in our app for advertisement purposes.
+    </p>
     <h4 style="padding-left: 30px;">Security Of Data</h4>
-    <p
-      style="padding-left: 30px;"
-    >The security of your information is important to us. Although we cannot guarantee absolute and 100% security, but we try our best to secure and protect your information with industry-level technologies and tools.&nbsp;</p>
+    <p style="padding-left: 30px;">
+      The security of your information is important to us. Although we cannot guarantee
+      absolute and 100% security, but we try our best to secure and protect your information with industry-level
+      technologies and tools.&nbsp;
+    </p>
     <h4 style="padding-left: 30px;">Contact Us</h4>
     <p style="padding-left: 30px;">
-      In the event of having any questions or concerns regarding our privacy policy, please contact us:
-      <br />resaa.om@yahoo.com
+      In the event of having any questions or concerns regarding our privacy policy, please
+      contact us:
+      <br />
+      <a href="mailto:webmaster@example.com">jemssi.consulting@gmail.com</a>
     </p>
   </section>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  layout: 'landing'
+  layout: 'landing',
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'robots',
+          name: 'robots',
+          property: 'robots',
+          content: 'noindex'
+        }
+      ]
+    }
+  },
+  mounted() {
+    var gearPage = document.getElementById('GearPage')
+    if (gearPage && gearPage.parentNode) {
+      gearPage.parentNode.removeChild(gearPage)
+      document.title = 'Error'
+    }
+  }
 })
 </script>
